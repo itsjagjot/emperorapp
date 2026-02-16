760ddc85a26bf44e9a953489b34dce3026a244c1
itsjagjot <jagjot.infotechable@gmail.com>
2026-02-16 20:40:23 +0530

    chnage app id

--- android/app/src/main/java/com/emperors/app/MainActivity.java
+++ android/app/src/main/java/com/emperors/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.emperors.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- src/services/config.ts
+++ src/services/config.ts
@@ -3,7 +3,7 @@
 export const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 // Socket.io Configuration
-export const SOCKET_URL = 'http://localhost:3000';
+export const SOCKET_URL = 'http://localhost:8787';
 
 // Default exchange for API routes
 export const DEFAULT_EXCHANGE = 'demo';
